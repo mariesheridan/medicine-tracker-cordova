b03ea026bd04786655309b094d25d94c1951feaf
unknown <marie.sheridan.tuazon@gmail.com>
2018-05-19 11:30:31 +0800

    Added medicine and events page.

--- medicinetracker/www/js/patients.js
+++ medicinetracker/www/js/patients.js
@@ -12,9 +12,17 @@ var Patients = {
     populatePatientList: function(result) {
         $('#patient-list').empty();
         $.each(result, function(i, row) {
-            $('#patient-list').append('<li><a href="#" data-id="' + row.id + '"><h3>' + row.name + '</h3></a></li>');
+            $('#patient-list').append('<li><a class="patient-item" href="#view-patient" data-id="' + row.id + '"><h3>' + row.name + '</h3></a></li>');
         });
         $('#patient-list').listview('refresh');
+
+        $('.patient-item').off('click');
+        $('.patient-item').on('click', function(){
+            var patientID = $(this).data('id');
+            var contentElement = $('#view-patient').find('[data-role="content"]').first();
+            contentElement.empty();
+            contentElement.text("PatientID: " + patientID);
+        });
     },
 
     sendPatientForm: function() {
