0820bb60624d7b8c303465f1982fc44a8c61ff1c
unknown <marie.sheridan.tuazon@gmail.com>
2018-05-22 22:23:43 +0800

    Empty old report when new report is generated.

--- medicinetracker/www/js/report.js
+++ medicinetracker/www/js/report.js
@@ -1,17 +1,10 @@
 var Report = {
     createReport: function(medicines, events) {
-        var container = $('#report').find('.content-holder').first()[0];
+        var contentHolder = $('#report').find('.content-holder').first();
+        contentHolder.empty();
+        var container = contentHolder[0];
         const MedicineGroupID = 1;
         const EventGroupID = 2;
-
-        // var items = new vis.DataSet([
-        //     {id: 1, content: 'item 1', start: '2013-04-20 08:00'},
-        //     {id: 2, content: 'item 2', start: '2013-04-20 07:00'},
-        //     {id: 3, content: 'item 3', start: '2013-04-18'},
-        //     {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
-        //     {id: 5, content: 'item 5', start: '2013-04-25'},
-        //     {id: 6, content: 'item 6', start: '2013-04-27'}
-        // ]);
 
         var itemsArray = [];
         var idCounter = 1;
